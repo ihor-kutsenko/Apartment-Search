d422c403b84d2d4fb165dff5410ce195285c8691
Rammbest <igor-kutsenko@ukr.net>
2023-12-14 22:42:44 +0200

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import "./scss/index.scss";
-import { createApp } from "vue";
-import App from "./App.vue";
+import './scss/index.scss';
+import { createApp } from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
-createApp(App).mount("#app");
+Vue.use(VueRouter);
+
+createApp(App).mount('#app');
